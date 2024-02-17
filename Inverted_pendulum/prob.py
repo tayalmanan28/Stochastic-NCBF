changes in signs of eta, minor errors corrected

Inverted_pendulum/prob.py
@@ -9,8 +9,8 @@ import math
 # set the super-rectangle range
 ############################################
 # set the initial in super-rectangle
-INIT = [[-1 / 15 * math.pi, 1 / 15 * math.pi], \
-            [- 1 / 15 * math.pi, 1 / 15 * math.pi]
+INIT = [[- math.pi/15, math.pi/15], \
+            [- math.pi/15, math.pi/15]
         ]
 INIT_SHAPE = 1 # 2 for circle, 1 for rectangle
 
@@ -97,6 +97,4 @@ def func_g(x):
     vf = torch.stack([g(i + 1) for i in range(superp.DIM_S)], dim=1)
     return vf
 
-L_x=1.1
-L_u=0.01
-#L_f=L_x+L_u*L_c
+L_x=1

loss.py
@@ -71,11 +71,11 @@ def calc_loss(barr_nn, x_safe, x_unsafe, x_domain, epoch, batch_index, eta,lip_h
     if h_safe.device != 'cpu':
         eta = eta.cuda(device)
         
-    loss_safe = torch.relu(-h_safe - superp.gamma + superp.TOL_SAFE + eta) #tolerance
+    loss_safe = torch.relu(-h_safe - superp.gamma + superp.TOL_SAFE -eta) #tolerance
 
     # compute loss of unsafe
     h_unsafe, d_h_unsafe, d2_h_unsafe = barr_nn(x_unsafe, hessian=True)
-    loss_unsafe = torch.relu(h_unsafe - superp.lamda - superp.TOL_UNSAFE - eta) #tolerance
+    loss_unsafe = torch.relu(h_unsafe - superp.lamda - superp.TOL_UNSAFE -eta) #tolerance
     
     # compute loss of domain
     h_domain, d_h_domain, d2_h_domain = barr_nn(x_domain, hessian=True)
@@ -86,15 +86,17 @@ def calc_loss(barr_nn, x_safe, x_unsafe, x_domain, epoch, batch_index, eta,lip_h
 
     f_x = prob.func_f(x_domain)
     g_x = prob.func_g(x_domain)
-    sigma = 0.1*torch.ones([2])
+    sigma = 0.0*torch.ones([2])
     gamma = 1
+
+    # print(d_h_domain)
 
     u, l = safe.calc_safe_u(x_domain, h_domain, d_h_domain, d2_h_domain,f_x, g_x,sigma, gamma)
         
     # vector_domain = prob.func_f(x_domain) # compute vector field at domain
     # print('Shape of del h & dynamics', h_domain.shape, d_h_domain.shape, d2_h_domain.shape)
     
-    loss_lie=torch.relu(-l.to(device) + superp.TOL_LIE + eta)
+    loss_lie=torch.relu(-l.to(device) + superp.TOL_LIE -eta)
         
     total_loss = superp.DECAY_SAFE * torch.sum(loss_safe) + superp.DECAY_UNSAFE * torch.sum(loss_unsafe) \
                     + superp.DECAY_LIE * torch.sum(loss_lie) #+ loss_eta

sysplot.py
@@ -16,22 +16,24 @@ import random
 
 mpl.rcParams.update(mpl.rcParamsDefault)
 
-barr_nn=torch.load('experiments/di_2_1_0.1_st/iterations/barr_nn_49')
+filename = 'experiments/ip_20'
+
+barr_nn=torch.load(filename+'/iterations/barr_nn_103')
 
 # s=20
-t=5000
+t=500
 
 # fig,axs = plt.subplots(3,1, figsize=(7, 15))
 
 x = torch.zeros([t,2])
 
-for i in range(t):
-    x[i,0] = random.uniform(-5.0, 5.0)
-    x[i,1] = random.uniform(-5.0, 5.0)
-
 # for i in range(t):
-#     x[i,0] = random.uniform(-math.pi/4, math.pi/4)
-#     x[i,1] = random.uniform(-math.pi/4, math.pi/4)
+#     x[i,0] = random.uniform(-5.0, 5.0)
+#     x[i,1] = random.uniform(-5.0, 5.0)
+
+for i in range(t):
+    x[i,0] = random.uniform(-math.pi/4, math.pi/4)
+    x[i,1] = random.uniform(-math.pi/4, math.pi/4)
 
 # x[:, 0] = torch.linspace(-5, 5, t)
 # x[:, 1] = torch.linspace(-5, 5, t)
@@ -41,27 +43,27 @@ h = h[:, 0, :]
 print(h)
 # print(x.shape, h.shape)
 for i in range(t):
-    if h[i]>= 0.00075:
+    if h[i]> 0.0005:
         plt.scatter(x[i,0], x[i,1], c='yellow', s=1)
-    elif h[i]>= -0.00075 and h[i] <= 0.00075:
+    elif h[i]< -0.000:
         plt.scatter(x[i,0], x[i,1], c='black', s=1)
     else:
         plt.scatter(x[i,0], x[i,1], c='red', s=1)
 
 
-currentAxis = plt.gca()
-currentAxis.add_patch(Rectangle((0 - 1, 0 - 1), 2, 2, fill=None, alpha=1))
-
-currentAxis2 = plt.gca()
-currentAxis2.add_patch(Rectangle((0 - 3, 0 - 3), 6, 6, fill=None, alpha=1))
-
 # currentAxis = plt.gca()
-# currentAxis.add_patch(Rectangle((0 - math.pi/15, 0 - math.pi/15), math.pi/7.5, math.pi/7.5, fill=None, alpha=1))
+# currentAxis.add_patch(Rectangle((0 - 1, 0 - 1), 2, 2, fill=None, alpha=1))
 
 # currentAxis2 = plt.gca()
-# currentAxis2.add_patch(Rectangle((0 - math.pi/6, 0 - math.pi/6), math.pi/3, math.pi/3, fill=None, alpha=1))
+# currentAxis2.add_patch(Rectangle((0 - 3, 0 - 3), 6, 6, fill=None, alpha=1))
 
-plt.savefig("experiments/di_2_1_0.1_st/barr_plot.png",dpi=1200)    
+currentAxis = plt.gca()
+currentAxis.add_patch(Rectangle((0 - math.pi/15, 0 - math.pi/15), math.pi/7.5, math.pi/7.5, fill=None, alpha=1))
+
+currentAxis2 = plt.gca()
+currentAxis2.add_patch(Rectangle((0 - math.pi/6, 0 - math.pi/6), math.pi/3, math.pi/3, fill=None, alpha=1))
+
+plt.savefig(filename+"/barr_plot.png",dpi=1200)    
 plt.show()    
 
 

train.py
@@ -58,11 +58,11 @@ def initialize_parameters(n_h_b, d_h_b):
     lambda_h=Variable(torch.normal(mean=10*torch.ones(n_h_b*d_h_b),std=0.001*torch.ones(n_h_b*d_h_b)), requires_grad=True)
     lambda_dh=Variable(torch.normal(mean=10*torch.ones(n_h_b*d_h_b),std=0.001*torch.ones(n_h_b*d_h_b)), requires_grad=True)
     print("Initialize eta")
-    eta=Variable(torch.normal(mean=torch.tensor([-0.00075]), std=torch.tensor([0.00001])), requires_grad=True)
+    eta=Variable(torch.normal(mean=torch.tensor([-0.0005]), std=torch.tensor([0.00001])), requires_grad=True)
     return lambda_h, lambda_dh, eta
 
     
-def initialize_nn(num_batches, lambda_h, lambda_dh):    
+def initialize_nn(num_batches, eta, lambda_h, lambda_dh):    
     print("Initialize nn parameters!")
     cuda_flag = True
     filename = f"barr_nn"
@@ -71,7 +71,7 @@ def initialize_nn(num_batches, lambda_h, lambda_dh):
     # Activation must be in ['ReLu', 'SoftPlus']
     hyper = {'n_width': superp.D_H_B,
              'n_depth': superp.N_H_B,
-             'learning_rate': 5.0e-04,
+             'learning_rate': 1.0e-03,
              'weight_decay': 1.e-6,
              'activation': "SoftPlus"}
 
@@ -80,11 +80,13 @@ def initialize_nn(num_batches, lambda_h, lambda_dh):
         # load_file = f"./models/{filename}.torch"
         # state = torch.load(load_file, map_location='cpu')
 
-        barr_nn = torch.load('experiments/di_2_1_0.1/iterations/barr_nn_200') #DifferentialNetwork(n_dof, **state['hyper'])
+        barr_nn = torch.load('experiments/ip_1_1_1_imp/iterations/barr_nn_490') #DifferentialNetwork(n_dof, **state['hyper'])
         # barr_nn.load_state_dict(state['state_dict'])
 
     else:
         barr_nn = DifferentialNetwork(n_dof, **hyper)
+        for p in barr_nn.parameters():
+            nn.init.normal_(p,0,0.1)
 
     if cuda_flag:
         barr_nn.cuda()
@@ -103,7 +105,7 @@ def initialize_nn(num_batches, lambda_h, lambda_dh):
 
 def itr_train(batches_safe, batches_unsafe, batches_domain, NUM_BATCHES, system):
     logger = DataLog()
-    log_dir = "experiments/" + system+"_1_1_1_st"
+    log_dir = "experiments/" + system+"_15"
     working_dir = os.getcwd()
 
     if os.path.isdir(log_dir) == False:
@@ -125,7 +127,7 @@ def itr_train(batches_safe, batches_unsafe, batches_domain, NUM_BATCHES, system)
         
         # initialize nn models and optimizers and schedulers
         lambda_h, lambda_dh, eta = initialize_parameters(superp.N_H_B, superp.D_H_B)
-        barr_nn, optimizer_barr, _ = initialize_nn(NUM_BATCHES[3], lambda_h, lambda_dh)
+        barr_nn, optimizer_barr, scheduler_barr = initialize_nn(NUM_BATCHES[3], eta, lambda_h, lambda_dh)
         optimizer_eta= torch.optim.SGD([{'params':[eta]}], lr=0.001, momentum=0)
 
 
@@ -172,7 +174,6 @@ def itr_train(batches_safe, batches_unsafe, batches_domain, NUM_BATCHES, system)
                 if curr_lmi_loss >= -5000:
                     curr_lmi_loss.backward()
                     optimizer_barr.step()
-                    
                     optimizer_barr.zero_grad()
                 
                 optimizer_eta.zero_grad()
@@ -183,6 +184,9 @@ def itr_train(batches_safe, batches_unsafe, batches_domain, NUM_BATCHES, system)
                     curr_eta_loss.backward()
                     optimizer_eta.step()
                 
+                # learning rate scheduling for each mini batch
+                scheduler_barr.step() # re-schedule learning rate once
+
                 # update epoch loss
                 lie_loss += lie_batch_loss.item()
                 epoch_loss += curr_batch_loss.item()